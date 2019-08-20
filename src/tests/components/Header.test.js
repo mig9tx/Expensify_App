f09e9efe322c1c898955be0912e8f7a135c5730f
mig9tx <miguel.trujillo@utexas.edu>
2019-08-20 18:12:34 -0500

    set up Enzyme to allow for testing of React components

---
+++ src/tests/components/Header.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import toJSON from 'enzyme-to-json';
+import Header from '../../components/Header';
+
+test('should render Header correctly', () => {
+    const wrapper = shallow(<Header />);
+    expect(toJSON(wrapper)).toMatchSnapshot();
+    
+    // Enzyme
+    // expect(wrapper.find('h1').length).toBe(1);
+    // expect(wrapper.find('h1').text()).toBe('Expensify');
+
+    // Renderer
+    // const renderer = new ReactShallowRenderer();
+    // renderer.render(<Header />);
+    // expect(renderer.getRenderOutput()).toMatchSnapshot();
+});
+
